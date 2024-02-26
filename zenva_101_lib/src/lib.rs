7dd7f85ebaf9d2dd3f1c7173ee07a578cc5fc0e3
K <kelseydrhoda@gmail.com>
2024-02-25 18:48:26 -0700

    Now has basic working enemy

--- zenva_101_lib/src/lib.rs
+++ zenva_101_lib/src/lib.rs
@@ -1,3 +1,4 @@
+use gdnative::api::Area2D;
 use gdnative::prelude::*;
 use godot_sane_defaults::kb2d_move_and_slide;
 
@@ -28,7 +29,7 @@ impl Player {
 
     #[method]
     fn _ready(&self, #[base] _base: &KinematicBody2D) {
-        godot_print!("Hello, Godot, from Rust!")
+        godot_print!("Hello, Godot, from Player!")
     }
 
     #[method]
@@ -38,10 +39,8 @@ impl Player {
         let input = Input::godot_singleton();
 
         if input.is_action_pressed("move_right", false) {
-            godot_print!("In right");
             self.velocity.x += self.speed;
         } else if input.is_action_pressed("move_left", false) {
-            godot_print!("In left");
             self.velocity.x -= self.speed;
         }
 
@@ -49,9 +48,7 @@ impl Player {
 
         self.velocity.y += GRAVITY as f32 * delta;
 
-        // if input.is_action_just_pressed("jump", false) {
         if input.is_action_just_pressed("jump", false) && base.is_on_floor() {
-            godot_print!("In Jump");
             self.velocity.y -= JUMP_FORCE as f32;
         }
 
@@ -64,9 +61,75 @@ impl Player {
     }
 }
 
+#[derive(NativeClass)]
+#[inherit(Area2D)]
+pub struct Enemy {
+    #[property(default = 100.0)]
+    speed: f32,
+    #[property(default = 100.0)]
+    move_distance: f32,
+    #[property(no_editor)]
+    start_position: f32,
+    #[property(no_editor)]
+    target_position: f32,
+}
+
+#[methods]
+impl Enemy {
+    fn new(_base: &Area2D) -> Self {
+        Enemy {
+            speed: 0.0,
+            move_distance: 0.0,
+            start_position: 0.0,
+            target_position: 0.0,
+        }
+    }
+
+    #[method]
+    fn _ready(&mut self, #[base] base: &Area2D) {
+        self.start_position = base.position().x;
+        self.target_position = self.start_position + self.move_distance;
+        godot_print!("Hello, Godot, from Enemy!")
+    }
+
+    fn move_to(current: f32, to: f32, step: f32) -> f32 {
+        let mut next = current;
+        if next < to {
+            next += step;
+            if next > to {
+                next = to;
+            }
+        } else {
+            next -= step;
+            if next < to {
+                next = to;
+            }
+        }
+
+        next
+    }
+
+    #[method]
+    fn _process(&mut self, #[base] base: &Area2D, delta: f32) {
+        base.set_position(Vector2::new(
+            Enemy::move_to(base.position().x, self.target_position, self.speed * delta),
+            base.position().y,
+        ));
+
+        if base.position().x == self.target_position {
+            if self.target_position == self.start_position {
+                self.target_position = base.position().x + self.move_distance;
+            } else {
+                self.target_position = self.start_position;
+            }
+        }
+    }
+}
+
 // Registers all exposed classes to Godot.
 fn init(handle: InitHandle) {
     handle.add_class::<Player>();
+    handle.add_class::<Enemy>();
 }
 
 // Creates entry-points of dyn lib.
